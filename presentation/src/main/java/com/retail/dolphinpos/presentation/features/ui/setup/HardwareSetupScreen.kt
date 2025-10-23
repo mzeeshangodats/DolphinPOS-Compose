a97948ad85e82942cf86f51bb3478406266d7213
Muhammad Zeeshan <zeeshanarif573@gmail.com>
2025-10-23 19:05:59 +0500

    Merge with main branch

--- presentation/src/main/java/com/retail/dolphinpos/presentation/features/ui/setup/HardwareSetupScreen.kt
+++ presentation/src/main/java/com/retail/dolphinpos/presentation/features/ui/setup/HardwareSetupScreen.kt
@@ -1,8 +1,14 @@
 package com.retail.dolphinpos.presentation.features.ui.setup
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -10,7 +16,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.retail.dolphinpos.common.components.BaseText
